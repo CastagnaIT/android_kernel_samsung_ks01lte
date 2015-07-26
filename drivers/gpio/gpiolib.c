d956fe7733f18406926b06efa6fa3d66dc7df241
Stefano Gottardo <gottardo.stefano.83@gmail.com>
2015-07-26 12:41:21 +0200

    Squashed update of kernel from 3.4.106 to 3.4.107 All these commmit are not applyed because cause malfunctions with samsung rom or kernel stuck at boot:
    288a07dd11b2fc0408fbcb2a6dc2f837dedfb3d8 mm: propagate error from stack expansion even for guard page
    0b4f2ae74a52418cd880d9249e65fc935f02e89a mm: prevent endless growth of anon_vma hierarchy
    7f1be7c6e55b686ee22b97db779eb7ed01a2f3af mm: protect set_page_dirty() from ongoing truncation
    df5f1b2e38a0f9d212c073502c4ba95feeeef33b mm: Don't count the stack guard page towards RLIMIT_STACK
    8463d31289d4c79e739798583bc5862c594c11cb mm: fix corner case in anon_vma endless growing prevention
    972dafe08e0434c3ce1caf5c86d7dae222d85588 mm: fix anon_vma->degree underflow in anon_vma endless growing prevention
    
    5e4c2b6bfdc7d2aa17efad3123ef894543981a59 gpiolib: Refactor gpio_export
    d08ee685d847a7f51ad271b089bbf9b2ee750c5c gpio: sysfs: fix gpio device-attribute leak
    d77c3bbf70dd9734b2031475813cd03932b7e6ed gpio: sysfs: fix gpio attribute-creation race

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -589,16 +589,13 @@ static ssize_t chip_ngpio_show(struct device *dev,
 }
 static DEVICE_ATTR(ngpio, 0444, chip_ngpio_show, NULL);
 
-static const struct attribute *gpiochip_attrs[] = {
+static struct attribute *gpiochip_attrs[] = {
 	&dev_attr_base.attr,
 	&dev_attr_label.attr,
 	&dev_attr_ngpio.attr,
 	NULL,
 };
-
-static const struct attribute_group gpiochip_attr_group = {
-	.attrs = (struct attribute **) gpiochip_attrs,
-};
+ATTRIBUTE_GROUPS(gpiochip);
 
 /*
  * /sys/class/gpio/export ... write-only
@@ -805,6 +802,7 @@ int gpio_export_link(struct device *dev, const char *name, unsigned gpio)
 		if (tdev != NULL) {
 			status = sysfs_create_link(&dev->kobj, &tdev->kobj,
 						name);
+			put_device(tdev);
 		} else {
 			status = -ENODEV;
 		}
@@ -855,7 +853,7 @@ int gpio_sysfs_set_active_low(unsigned gpio, int value)
 	}
 
 	status = sysfs_set_active_low(desc, dev, value);
-
+	put_device(dev);
 unlock:
 	mutex_unlock(&sysfs_lock);
 
@@ -924,13 +922,13 @@ static int gpiochip_export(struct gpio_chip *chip)
 
 	/* use chip->base for the ID; it's already known to be unique */
 	mutex_lock(&sysfs_lock);
-	dev = device_create(&gpio_class, chip->dev, MKDEV(0, 0), chip,
-				"gpiochip%d", chip->base);
-	if (!IS_ERR(dev)) {
-		status = sysfs_create_group(&dev->kobj,
-				&gpiochip_attr_group);
-	} else
+	dev = device_create_with_groups(&gpio_class, chip->dev, MKDEV(0, 0),
+					chip, gpiochip_groups,
+					"gpiochip%d", chip->base);
+	if (IS_ERR(dev))
 		status = PTR_ERR(dev);
+	else
+		status = 0;
 	chip->exported = (status == 0);
 	mutex_unlock(&sysfs_lock);
 
@@ -1081,23 +1079,22 @@ int gpiochip_add(struct gpio_chip *chip)
 				? (1 << FLAG_IS_OUT)
 				: 0;
 		}
-	}
 
 #ifdef CONFIG_PINCTRL
 	INIT_LIST_HEAD(&chip->pin_ranges);
 #endif
 
-	of_gpiochip_add(chip);
+		of_gpiochip_add(chip);
+	}
 
 unlock:
 	spin_unlock_irqrestore(&gpio_lock, flags);
 
-	if (status)
-		goto fail;
-
 	status = gpiochip_export(chip);
-	if (status)
+	if (status) {
+		of_gpiochip_remove(chip);
 		goto fail;
+	}
 
 	pr_info("gpiochip_add: registered GPIOs %d to %d on device: %s\n",
 		chip->base, chip->base + chip->ngpio - 1,
